8eb3ef4229f9491107e323c198e4d2cd7e236ea4
InfiniteSports <156534968+InfiniteSports@users.noreply.github.com>
2024-03-12 20:04:08 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -4,8 +4,6 @@ class Config:
     API_ID = int(getenv("API_ID", "2238786"))
     API_HASH = getenv("API_HASH", "e449d6cc630583d0b415b286eedb9192")
     BOT_TOKEN = getenv("BOT_TOKEN", "6900717691:AAHTC2hKMhNjEJqEKUIbsE-VkdnGB6a5dMA")
-    FSUB = getenv("FSUB", "PS_BOTz")
-    CHID = int(getenv("CHID", "-1001910352559"))
     SUDO = list(map(int, getenv("SUDO", "950958796 5003133371").split()))
     MONGO_URI = getenv("MONGO_URI", "mongodb+srv://Auto:Auto@cluster0.xnmdjg4.mongodb.net/?retryWrites=true&w=majority")
     
